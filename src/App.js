126a1e821745b31587a085dbf84f09ac485b19f3
cho-jin-woo <chlwlsdnzzzz@naver.com>
2024-08-02 18:15:08 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Features from './components/Features';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Signin from './components/Auth/Signin';
+import Signup from './components/Auth/Signup';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/features" element={<Features />} />
+
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/components/Home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import HeroSection from './HeroSection';
+import FeaturesSection from './FeaturesSection';
+
+const index = () => {
+  return (
+    <div>
+      <div className="container">
+        <HeroSection />
+        <FeaturesSection />
+      </div>
+    </div>
+  );
+};
+
+export default index;
